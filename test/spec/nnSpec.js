a802b9c6263c2820eb6a21ee474ea334f6627b44
jasonmcaffee <jasonlmcaffee@gmail.com>
2013-10-27 19:15:36 -0400

    documentation

--- test/spec/nnSpec.js
+++ test/spec/nnSpec.js
@@ -1,42 +1,42 @@
-describe("nn", function() {
-	'use strict';
+describe("nn", function () {
+    'use strict';
 
-	//setup
-	var obj;
-	beforeEach(function() {
-		obj = {
-			prop1: 'a', 
-			prop2: {
-				prop2_1: 'b',
-                func2:function(param1, param2, param3){
+    //setup
+    var obj;
+    beforeEach(function () {
+        obj = {
+            prop1: 'a',
+            prop2: {
+                prop2_1: 'b',
+                func2: function (param1, param2, param3) {
                     return this.prop2_1 + param1 + param2 + param3;
                 }
-			},
-			prop3: {
-				prop3_1:{
-					prop3_1_1: 'c'
-				}
-			},
-			prop4:[
-				'd', 
-				1, 
-				{
-					prop4_2: {
-						prop4_2_1: {
-							prop4_2_1_1: 'e'
-						}
-					}
-				}
-			],
-			undef: undefined,
-			nul: null,
-			func1: function(){
-				return this.prop1;
-			}
-		};
-	});
+            },
+            prop3: {
+                prop3_1: {
+                    prop3_1_1: 'c'
+                }
+            },
+            prop4: [
+                'd',
+                1,
+                {
+                    prop4_2: {
+                        prop4_2_1: {
+                            prop4_2_1_1: 'e'
+                        }
+                    }
+                }
+            ],
+            undef: undefined,
+            nul: null,
+            func1: function () {
+                return this.prop1;
+            }
+        };
+    });
 
-    function timed(f){
+    function timed(f) {
         var start = new Date().getTime();
         f();
         var end = new Date().getTime();
@@ -46,127 +46,128 @@ describe("nn", function() {
     }
 
 
-	it("should be able to access property values of object literals by using chained function calls", function() {
-		var prop1 = nn(obj)('prop1').val;
-		expect(prop1).toEqual(obj.prop1); 
+    it("should be able to access property values of object literals by using chained function calls", function () {
+        var prop1 = nn(obj)('prop1').val;
+        expect(prop1).toEqual(obj.prop1);
 
-		var prop2 = nn(obj)('prop2').val;
-		expect(prop2).toEqual(obj.prop2);
+        var prop2 = nn(obj)('prop2').val;
+        expect(prop2).toEqual(obj.prop2);
 
-		var prop2_1 = nn(obj)('prop2')('prop2_1').val;
-		expect(prop2_1).toEqual(obj.prop2.prop2_1);
+        var prop2_1 = nn(obj)('prop2')('prop2_1').val;
+        expect(prop2_1).toEqual(obj.prop2.prop2_1);
 
-		var prop3_1_1 = nn(obj)('prop3')('prop3_1')('prop3_1_1').val;
-		expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
+        var prop3_1_1 = nn(obj)('prop3')('prop3_1')('prop3_1_1').val;
+        expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
 
         var nul = nn(obj)('nul').val;
         expect(nul).toEqual(obj.nul);
-	});
+    });
 
-	it("should be able to access property values of object literals by using . notation selectors", function() {
-		var prop2_1 = nn(obj)('prop2.prop2_1').val;
-		expect(prop2_1).toEqual(obj.prop2.prop2_1);
+    it("should be able to access property values of object literals by using . notation selectors", function () {
+        var prop2_1 = nn(obj)('prop2.prop2_1').val;
+        expect(prop2_1).toEqual(obj.prop2.prop2_1);
 
-		var prop3_1_1 = nn(obj)('prop3.prop3_1.prop3_1_1').val;
-		expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
-	});
+        var prop3_1_1 = nn(obj)('prop3.prop3_1.prop3_1_1').val;
+        expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
+    });
 
-	it("should be able to access property values of object literals by using a mix of . notation selectors and chained function calls", function() {
-		var prop3_1_1 = nn(obj)('prop3.prop3_1')('prop3_1_1').val;
-		expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
+    it("should be able to access property values of object literals by using a mix of . notation selectors and chained function calls", function () {
+        var prop3_1_1 = nn(obj)('prop3.prop3_1')('prop3_1_1').val;
+        expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
 
-		prop3_1_1 = nn(obj)('prop3')('prop3_1.prop3_1_1').val;
-		expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
-	});
+        prop3_1_1 = nn(obj)('prop3')('prop3_1.prop3_1_1').val;
+        expect(prop3_1_1).toEqual(obj.prop3.prop3_1.prop3_1_1);
+    });
 
-	it("should be able to access property values of undefined or null properties and the val should be undefined", function() {
-		var undef = nn(obj)('does')('not.exist').val;
-		expect(undef).toEqual(undefined);
-	});
+    it("should be able to access property values of undefined or null properties and the val should be undefined", function () {
+        var undef = nn(obj)('does')('not.exist').val;
+        expect(undef).toEqual(undefined);
+    });
 
-	it("should be able to handle null or undefined query objects", function(){
-		var undef = nn(null)('does')('not')('exist').val;
-		expect(undef).toEqual(undefined);
+    it("should be able to handle null or undefined query objects", function () {
+        var undef = nn(null)('does')('not')('exist').val;
+        expect(undef).toEqual(undefined);
 
-		undef = nn(undefined)('does')('not')('exist').val;
-		expect(undef).toEqual(undefined);
-	});
+        undef = nn(undefined)('does')('not')('exist').val;
+        expect(undef).toEqual(undefined);
+    });
 
-	it("should be able to handle null or undefined selectors", function(){
-		var undef = nn(obj)(undefined)(null)(undefined).val;
-		expect(undef).toEqual(undefined);
+    it("should be able to handle null or undefined selectors", function () {
+        var undef = nn(obj)(undefined)(null)(undefined).val;
+        expect(undef).toEqual(undefined);
 
-		undef = nn(null)(undefined)(null)(undefined).val;
-		expect(undef).toEqual(undefined);
-	});
+        undef = nn(null)(undefined)(null)(undefined).val;
+        expect(undef).toEqual(undefined);
+    });
 
-	it("should be able to access array items with a number selector", function(){
-		var prop4_0 = nn(obj)('prop4')(0).val;
-		expect(prop4_0).toEqual(obj.prop4[0]);
+    it("should be able to access array items with a number selector", function () {
+        var prop4_0 = nn(obj)('prop4')(0).val;
+        expect(prop4_0).toEqual(obj.prop4[0]);
 
-		var prop4_1 = nn(obj)('prop4')(1).val;
-		expect(prop4_1).toEqual(obj.prop4[1]);
+        var prop4_1 = nn(obj)('prop4')(1).val;
+        expect(prop4_1).toEqual(obj.prop4[1]);
 
-		var prop4_2 = nn(obj)('prop4')(2).val;
-		expect(prop4_2).toEqual(obj.prop4[2]);
+        var prop4_2 = nn(obj)('prop4')(2).val;
+        expect(prop4_2).toEqual(obj.prop4[2]);
 
         var undef = nn(obj)('prop4')(999).val;
         expect(undef).toEqual(undefined);
 
         var undef = nn(obj)('prop4')(undefined).val;
         expect(undef).toEqual(undefined);
-	});
+    });
 
-	it("should be able to access array items with a number selector and continue to select", function(){
+    it("should be able to access array items with a number selector and continue to select", function () {
 
-		var prop4_2 = nn(obj)('prop4')(2)('prop4_2').val;
-		expect(prop4_2).toEqual(obj.prop4[2].prop4_2);
+        var prop4_2 = nn(obj)('prop4')(2)('prop4_2').val;
+        expect(prop4_2).toEqual(obj.prop4[2].prop4_2);
 
-		var prop4_2_1 = nn(obj)('prop4')(2)('prop4_2.prop4_2_1').val;
-		expect(prop4_2_1).toEqual(obj.prop4[2].prop4_2.prop4_2_1);
-	});
+        var prop4_2_1 = nn(obj)('prop4')(2)('prop4_2.prop4_2_1').val;
+        expect(prop4_2_1).toEqual(obj.prop4[2].prop4_2.prop4_2_1);
+    });
 
-    it("should allow functions to be executed with their original context", function(){
+    it("should allow functions to be executed with their original context", function () {
         var result = nn(obj)('func1').function();
         expect(result).toEqual(obj.prop1);
     });
 
-    it("should allow functions to be executed with parameters", function(){
-       var nnResult =  nn(obj)('prop2.func2').function('test', 'passing', 'params');
-       var nnResult2 = nn(obj)('prop2')('func2').function('test', 'passing', 'params');
-       var normalResult = obj.prop2.func2('test', 'passing', 'params');
-       expect(nnResult).toEqual(normalResult);
-       expect(nnResult2).toEqual(normalResult);
+    it("should allow functions to be executed with parameters", function () {
+        var nnResult = nn(obj)('prop2.func2').function('test', 'passing', 'params');
+        var nnResult2 = nn(obj)('prop2')('func2').function('test', 'passing', 'params');
+        var normalResult = obj.prop2.func2('test', 'passing', 'params');
+        expect(nnResult).toEqual(normalResult);
+        expect(nnResult2).toEqual(normalResult);
     });
 
-    it("should be able to execute undefined functions", function(){
+    it("should be able to execute undefined functions", function () {
         var result = nn(obj)('nonExistingFunc').function(123);
         expect(result).toEqual(undefined);
     });
 
     //this does not accurately reflect browser performance, and is relative to my machine. you can probably ignore this...
-    it("should be relatively fast", function(){
+    it("should be relatively fast", function () {
         var result, iterations = 100000, normalTime, nnTime;
-        function normal(){
-            if(obj && obj.prop3 && obj.prop3.prop3_1){
+
+        function normal() {
+            if (obj && obj.prop3 && obj.prop3.prop3_1) {
                 result = obj.prop3.prop3_1.prop3_1_1;
             }
         }
 
-        function usingNN(){
+        function usingNN() {
             result = nn(obj)('prop3.prop3_1.prop3_1_1').val;
         }
 
-        normalTime = timed(function(){
-            for(var i = 0; i < iterations; ++i){
+        normalTime = timed(function () {
+            for (var i = 0; i < iterations; ++i) {
                 normal();
             }
             expect(result).toEqual('c');
             result = null;
         });
 
-        nnTime = timed(function(){
-            for(var i = 0; i < iterations; ++i){
+        nnTime = timed(function () {
+            for (var i = 0; i < iterations; ++i) {
                 usingNN();
             }
             expect(result).toEqual('c');
