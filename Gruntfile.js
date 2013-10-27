b5113e9f097cd11571952e6be39db8126d538056
jasonmcaffee <jasonlmcaffee@gmail.com>
2013-10-27 16:45:01 -0400

    added templating for js files (build directives)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,76 @@
-module.exports = function(grunt){
-    var buildVersion = "0.0.2",
+module.exports = function (grunt) {
+    //modules
+    var handlebars = require('handlebars');
+
+    var buildVersion = "0.0.3",
         minFileName = "nn-" + buildVersion + ".min.js",
-        distFilePath = 'dist/' + minFileName,
+        nnCoreTemplateSrcFilePath = 'src/nn.hbs.js',
+        nnCoreDistFilePath = 'dist/' + minFileName,
         uglifyConfig = {};
-    uglifyConfig[distFilePath] = ['src/nn.js'];
+    //uglify from the dist to the dist.
+    uglifyConfig[nnCoreDistFilePath] = [nnCoreDistFilePath];
 
-	grunt.initConfig({
-	  jasmine: {
-		src: 'src/**/*.js',
-		options: {
-			specs: 'test/spec/*Spec.js',
-			helpers: 'test/spec/*Helper.js'
-		}
-	  },
-	  watch : {
-		scripts: {
-			files: ['src/**/*.js'],
-			tasks: ['test']
-		},
-		specs: {
-			files: ['test/spec/**/*.js'],
-			tasks: ['test']
-		}
-      },
-      uglify:{
-          options:{
-            report: 'gzip'
-          },
-          nn:{files:uglifyConfig}
-      }
-	});
+    grunt.initConfig({
+        nn:{
+            core:{
+                templateSrcFilePath: nnCoreTemplateSrcFilePath,
+                distFilePath: nnCoreDistFilePath,
+                templateData:{
+                    test: true
+                }
+            }
+        },
+        jasmine: {
+            nnCore:{
+                src: nnCoreDistFilePath,
+                options: {
+                    specs: 'test/spec/*Spec.js',
+                    helpers: 'test/spec/*Helper.js'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['build-nn-core']
+            },
+            specs: {
+                files: ['test/spec/**/*.js'],
+                tasks: ['test']
+            }
+        },
+        uglify: {
+            options: {
+                report: 'gzip'
+            },
+            nnCore: {files: uglifyConfig}
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    /**
+     * Compiles js templates in src (e.g. nn.hbs.js).
+     */
+    grunt.registerTask('compile-nn-core-templates',
+        'the src js is a template so we can pick and choose what to include in the dist file', function(){
+        var config = grunt.config('nn');
+
+        //build core nn.js
+        var coreConfig = config.core;
+        var nnCoreTemplate = grunt.file.read(coreConfig.templateSrcFilePath);
+        var nnCoreTemplateFunction = handlebars.compile(nnCoreTemplate);
+        var nnCoreJs = nnCoreTemplateFunction(coreConfig.templateData);
+        grunt.file.write(coreConfig.distFilePath, nnCoreJs);
+    });
+
+    grunt.registerTask('compile-nn-templates', ['compile-nn-core-templates']);
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-	
-	grunt.registerTask('test', ['jasmine']);
-	grunt.registerTask('default', ['test', 'uglify']);
+
+    grunt.registerTask('test', ['jasmine']);
+    grunt.registerTask('build-nn-core', ['compile-nn-core-templates', 'jasmine:nnCore']);
+    grunt.registerTask('build-nn-core-and-minify', ['build-nn-core', 'uglify:nnCore']);
+
+    grunt.registerTask('default', ['compile-nn-templates', 'test', 'uglify']);
 };
--- src/nn.hbs.js
+++ src/nn.hbs.js
@@ -1,44 +1,48 @@
-(function(){
-	'use strict';
-	var numberType = typeof 0,
-		stringType = typeof "",
-        emptyFunction = function(){}, //needed for undefined function execution
+(function () {
+    'use strict';
+    var numberType = typeof 0,
+        stringType = typeof "",
+        emptyFunction = function () {
+        }, //needed for undefined function execution
         functionType = typeof emptyFunction,
-		splitter = ".";
+        splitter = ".";
 
-	/**
+    /**
      * TODO: try providing a select that takes n params "prop1", "prop1_1", "prop2" if performance improves because of it.
-	* Recursive query select function generator.
-	* @returns {function} - closure bound to the last context
-	*/
-	function select(selector, context){
+     * Recursive query select function generator.
+     * @returns {function} - closure bound to the last context
+     */
+    function select(selector, context) {
         var selectorType = typeof selector,
-        	dotSplit,
-        	propertyName,
+            dotSplit,
+            propertyName,
             previousContext; //needed for function execution
 
         //determine what to do based on the selector type.
-        if(selectorType === stringType){
-        	dotSplit = selector.split(splitter); 
-        }else if(selectorType === numberType){
-        	dotSplit = [selector];
-        }else{
-        	context = null;//handle undefined selectors. e.g. nn(obj)(undefined)
+        if (selectorType === stringType) {
+            dotSplit = selector.split(splitter);
+        } else if (selectorType === numberType) {
+            dotSplit = [selector];
+        } else {
+            context = null;//handle undefined selectors. e.g. nn(obj)(undefined)
         }
 
+
         //iterate over each property and traverse contexts.
-        for(var i = 0; dotSplit && i < dotSplit.length; ++i){
+        for (var i = 0; dotSplit && i < dotSplit.length; ++i){
             propertyName = dotSplit[i];
             previousContext = context;
             context = context ? context[propertyName] : undefined; //traverse contexts
         }
 
         //closure bound to the last context
-        var result = function(s){ return select(s, context); };
+        var result = function (s) {
+            return select(s, context);
+        };
         result.val = context; //allow access to the last value. this is not protected (it can be null or undefined)
         //to allow functions to be executed safely, we provide the function which will call the real function if it exists,
         //passing it the arguments and the context of the real function's parent.
-        result.function = function(){
+        result.function = function () {
             var potentialFunc = previousContext && previousContext[propertyName] ? previousContext[propertyName] : undefined;
             var isFunc = typeof potentialFunc === functionType;
             return isFunc ? potentialFunc.apply(previousContext, arguments) : undefined;
@@ -47,14 +51,16 @@
     }
 
     /**
-    * Initialization function
-    * @param {object} obj - the query object. 
-    * @returns {function} - closure bound to the query object.
-    */
-	function nn(obj){
-		return function(sel){ return select(sel, obj); }
-	}
+     * Initialization function
+     * @param {object} obj - the query object.
+     * @returns {function} - closure bound to the query object.
+     */
+    function nn(obj) {
+        return function (sel) {
+            return select(sel, obj);
+        }
+    }
 
-	//assign nn to the global scrope.
-	window.nn = nn;
+    //assign nn to the global scrope.
+    window.nn = nn;
 })();
