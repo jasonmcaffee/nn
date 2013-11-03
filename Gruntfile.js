afc972a433930a9b9c81d0e90b85d366a92cf9af
jasonmcaffee <jasonlmcaffee@gmail.com>
2013-11-02 20:40:34 -0400

    performance optimizations. 1 million ops per second for cached nnObj. 825 thousand without cached.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,11 +46,25 @@ module.exports = function (grunt) {
             options: {
                 //http://lisperator.net/uglifyjs/compress
                 compress:{
-                    conditionals:false,
-                    loops:false,
-                    comparisons: false,
-                    booleans: false,
-                    if_return: false
+                    sequences     : false,  // join consecutive statemets with the “comma operator”
+                    properties    : false,  // optimize property access: a["foo"] → a.foo
+                    dead_code     : true,  // discard unreachable code
+                    drop_debugger : true,  // discard “debugger” statements
+                    unsafe        : false, // some unsafe optimizations (see below)
+                    conditionals  : false,  // optimize if-s and conditional expressions
+                    comparisons   : false,  // optimize comparisons
+                    evaluate      : false,  // evaluate constant expressions
+                    booleans      : false,  // optimize boolean expressions
+                    loops         : false,  // optimize loops
+                    unused        : true,  // drop unused variables/functions
+                    hoist_funs    : false,  // hoist function declarations
+                    hoist_vars    : false, // hoist variable declarations
+                    if_return     : false,  // optimize if-s followed by return/continue
+                    join_vars     : false,  // join var declarations
+                    cascade       : false,  // try to cascade `right` into `left` in sequences
+                    side_effects  : true,  // drop side-effect-free statements
+                    warnings      : true,  // warn about potentially dangerous optimizations/code
+                    global_defs   : {}     // global definitions
                 },
 
                 report: 'gzip'
