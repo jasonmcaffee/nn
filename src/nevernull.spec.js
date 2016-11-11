fccf50131377123a3f63745c353ae398fbb71055
jasonlmcaffee@gmail.com <jasonlmcaffee@gmail.com>
2016-11-10 21:22:32 -0700

    more tests added

--- src/nevernull.spec.js
+++ src/nevernull.spec.js
@@ -1,4 +1,4 @@
-import {nn} from './nevernull';
+const nn = require('./nevernull');
 
 describe("nevernull", ()=>{
 
@@ -14,6 +14,10 @@ describe("nevernull", ()=>{
     expect(testObject.a()).toEqual(undefined);
     expect(testObject.b.c()).toEqual(undefined);
     expect(testObject.d.e.f.g.h.i.j()).toEqual(undefined);
+
+    let undefinedObject;
+    testObject = nn(undefinedObject);
+    expect(testObject()).toEqual(undefinedObject);
   });
 
   it("should allow access to original values of properties which are defined", ()=>{
@@ -36,5 +40,39 @@ describe("nevernull", ()=>{
     expect( testObject.e()() ).toEqual(mockObject.e());
     expect( testObject.f()() ).toEqual(mockObject.f());
   });
+
+  it("should reflect changes made to underlying object", ()=>{
+    let mockObject = {
+      a: {
+        b: 123
+      }
+    };
+
+    let testObject = nn(mockObject);
+
+    mockObject.a.b = 456;
+    expect(testObject.a.b()).toEqual(mockObject.a.b);
+
+    mockObject.a = {b: 345};
+    expect(testObject.a.b()).toEqual(mockObject.a.b);
+  });
+
+  it("should change underlying object when changes are made to nn function-object", ()=>{
+    let mockObject = {
+      a: {
+        b: 'string'
+      }
+    };
+
+    let testObject = nn(mockObject);
+
+    testObject.a().b = 'another string';
+    expect(testObject.a.b()).toEqual(mockObject.a.b);
+
+    testObject().a = {b:'yet another string'};
+    expect(testObject.a.b()).toEqual(mockObject.a.b);
+  });
+
+
 
 });
