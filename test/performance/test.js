abdaedd03221282e227b97faff0ab89a114200e0
Jason McAffee <jasonlmcaffee@gmail.com>
2016-11-20 15:37:45 -0700

    performance test cleanup.

--- test/performance/test.js
+++ test/performance/test.js
@@ -9,13 +9,19 @@ if(!iterationsToRun){
 console.log(`performance test of ${iterationsToRun} iterations starting...`);
 
 const timeThis = functionToTime =>{
-    let start = process.hrtime();//Date.now();
+    let start = process.hrtime();
     functionToTime();
-    //let end = Date.now();
-    //let duration = end - start;
     let end = process.hrtime(start);
     let duration = end[0] * 1e9 + end[1];
     return duration;
+};
+
+const measureMemoryUsageInKB = (functionToGatherMemoryUsageFor, funcWhichAcceptsResults=(functionToGatherMemoryUsageForResult, memoryUsageResult)=>0) =>{
+    let start = process.memoryUsage().heapUsed;
+    let resultFromFunctionToGatherMemoryUsageFor = functionToGatherMemoryUsageFor();
+    let totalMemoryUsedInKB = (process.memoryUsage().heapUsed - start) / 1024;
+    gc();
+    funcWhichAcceptsResults(resultFromFunctionToGatherMemoryUsageFor, totalMemoryUsedInKB);
 };
 
 let example = {
@@ -75,61 +81,73 @@ const timeTotal = (timeReturningFunction, iterations) =>{
 };
 
 const runPerfTests = (iterations) =>{
-    let traditional5layersDeepMemUsageStart = process.memoryUsage().heapUsed;
-    let traditionalTimeInNano5LayersDeep =  timeTotal(timeTraditionalSafeguardedAccess5LayersDeep, iterations);
-    let traditional5layersDeepMemUsage = process.memoryUsage().heapUsed - traditional5layersDeepMemUsageStart;
-    gc();
-
-    let nevernull5LayersDeepMemUsageStart = process.memoryUsage().heapUsed;
-    let nevernullTimeInNano5LayersDeep = timeTotal(timeNevernullSafeguardedAccess5LayersDeep, iterations);
-    let nevernull5LayersDeepMemUsage = process.memoryUsage().heapUsed - nevernull5LayersDeepMemUsageStart;
-    gc();
-
-    let percentageMoreMemoryUsedNevernull5LayersDeep = Math.floor(traditional5layersDeepMemUsage / nevernull5LayersDeepMemUsage * 100);
-    let percentageFasterOfTraditional5LayersDeep = Math.floor(nevernullTimeInNano5LayersDeep / traditionalTimeInNano5LayersDeep * 100);
-
-    let traditionalTimeInNano3LayersDeep = timeTotal(timeTraditionalSafeguardedAccess3LayersDeep, iterations);
-    let nevernullTimeInNano3LayersDeep = timeTotal(timeNevernullSafeguardedAccess3LayersDeep, iterations);
-    let percentageFasterOfTraditional3LayersDeep = Math.floor(nevernullTimeInNano3LayersDeep / traditionalTimeInNano3LayersDeep * 100);
+    const fiveLayersOfNestingLabel = "5 layers of nesting";
+    const threeLayersOfNestingLabel = "3 layers of nesting";
+    const traditionalKbLabel = "total KB memory used for traditional";
+    const nnKbLabel = "total KB memory used for nevernull";
 
     let totalTimes = {
         nanoseconds:{
-            "3 layers of nesting":{
-                "total nano for traditional " : traditionalTimeInNano3LayersDeep,
-                "total nano for nevernull " : nevernullTimeInNano3LayersDeep
-            },
-            "5 layers of nesting":{
-                "total nano for traditional " : traditionalTimeInNano5LayersDeep,
-                "total nano for nevernull " : nevernullTimeInNano5LayersDeep
-            }
-
+            [fiveLayersOfNestingLabel]:{},
+            [threeLayersOfNestingLabel]:{}
         },
         milliseconds:{
-            "3 layers of nesting": {
-                "total ms for traditional ": traditionalTimeInNano3LayersDeep / 1000000,
-                "total ms for nevernull ": nevernullTimeInNano3LayersDeep / 1000000
-            },
-            "5 layers of nesting": {
-                "total ms for traditional ": traditionalTimeInNano5LayersDeep / 1000000,
-                "total ms for nevernull ": nevernullTimeInNano5LayersDeep / 1000000
-            }
+            [fiveLayersOfNestingLabel]:{},
+            [threeLayersOfNestingLabel]:{}
         },
         percentage:{
-            "3 layers of nesting":{
-                "traditional is faster by ": percentageFasterOfTraditional3LayersDeep + '%'
-            },
-            "5 layers of nesting":{
-                "traditional is faster by ": percentageFasterOfTraditional5LayersDeep + '%'
-            }
+            [fiveLayersOfNestingLabel]:{},
+            [threeLayersOfNestingLabel]:{}
         }
-
+    };
+    let memoryUsage = {
+        [fiveLayersOfNestingLabel]:{
+            [traditionalKbLabel]:"",
+            [nnKbLabel]:""
+        },
+        [threeLayersOfNestingLabel]:{
+            [traditionalKbLabel]:"",
+            [nnKbLabel]:""
+        }
     };
 
-    let memoryUsage = {
-        "5 layers of nesting":{
-            "total KB memory used for traditional": traditional5layersDeepMemUsage / 1024,
-            "total KB memory used for nevernull": nevernull5LayersDeepMemUsage / 1024
-        }
+    measureMemoryUsageInKB(()=>timeTotal(timeTraditionalSafeguardedAccess5LayersDeep, iterations), (timeTotalResult, memoryUsageResult)=>{
+        totalTimes.nanoseconds[fiveLayersOfNestingLabel]["total nano for traditional"] = timeTotalResult;
+        totalTimes.milliseconds[fiveLayersOfNestingLabel]["total ms for traditional"] = timeTotalResult / 1000000;
+        memoryUsage[fiveLayersOfNestingLabel][traditionalKbLabel] = memoryUsageResult;
+    });
+
+    measureMemoryUsageInKB(()=>timeTotal(timeNevernullSafeguardedAccess5LayersDeep, iterations), (timeTotalResult, memoryUsageResult)=>{
+        totalTimes.nanoseconds[fiveLayersOfNestingLabel]["total nano for nevernull"] = timeTotalResult;
+        totalTimes.milliseconds[fiveLayersOfNestingLabel]["total ms for nevernull"] = timeTotalResult / 1000000;
+        memoryUsage[fiveLayersOfNestingLabel][nnKbLabel] = memoryUsageResult;
+    });
+
+    measureMemoryUsageInKB(()=>timeTotal(timeTraditionalSafeguardedAccess3LayersDeep, iterations), (timeTotalResult, memoryUsageResult)=>{
+        totalTimes.nanoseconds[threeLayersOfNestingLabel]["total nano for traditional"] = timeTotalResult;
+        totalTimes.milliseconds[threeLayersOfNestingLabel]["total ms for traditional"] = timeTotalResult / 1000000;
+        memoryUsage[threeLayersOfNestingLabel][traditionalKbLabel] = memoryUsageResult;
+    });
+
+    measureMemoryUsageInKB(()=>timeTotal(timeNevernullSafeguardedAccess3LayersDeep, iterations), (timeTotalResult, memoryUsageResult)=>{
+        totalTimes.nanoseconds[threeLayersOfNestingLabel]["total nano for nevernull"] = timeTotalResult;
+        totalTimes.milliseconds[threeLayersOfNestingLabel]["total ms for nevernull"] = timeTotalResult / 1000000;
+        memoryUsage[threeLayersOfNestingLabel][nnKbLabel] = memoryUsageResult;
+    });
+
+    let threeLayersTraditionalNano = totalTimes.nanoseconds[threeLayersOfNestingLabel]["total nano for traditional"];
+    let threeLayersNevernullNano = Math.floor(totalTimes.nanoseconds[threeLayersOfNestingLabel]["total nano for nevernull"]);
+
+    totalTimes.percentage[threeLayersOfNestingLabel] = {
+        "traditional is faster by ":  Math.floor(threeLayersNevernullNano / threeLayersTraditionalNano * 100)
+    };
+
+
+    let fiveLayersTraditionalNano = totalTimes.nanoseconds[fiveLayersOfNestingLabel]["total nano for traditional"];
+    let fiveLayersNevernullNano = Math.floor(totalTimes.nanoseconds[fiveLayersOfNestingLabel]["total nano for nevernull"]);
+
+    totalTimes.percentage[fiveLayersOfNestingLabel] = {
+        "traditional is faster by ":  Math.floor(fiveLayersNevernullNano / fiveLayersTraditionalNano * 100)
     };
 
     console.log(JSON.stringify(totalTimes, null, 2));
@@ -137,4 +155,3 @@ const runPerfTests = (iterations) =>{
 };
 
 runPerfTests(iterationsToRun);
-
